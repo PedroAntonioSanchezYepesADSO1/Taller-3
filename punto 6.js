60fd3a13553e7a4f3c277d60243d9e9592dc4943
PedroAntonioSanchezYepesADSO101~ <yepeseyt2@gmail.com>
2022-10-20 22:33:51 -0500

    punto 6

---
+++ punto 6.js
@@ -0,0 +1,17 @@
+// 6. Considere dos variables llamadas temperatura y presión. Escriba una sentencia if-else que muestre
+    // en pantalla la palabra Alarma si la variable presión es mayor a 200 o si la variable temperatura es
+    // mayor a 100. En caso contrario, se debe mostrar en pantalla la palabra Normal.
+    let temperatura = Number(prompt('Ingrese el valor de la temperatura '))
+    let presion = Number(prompt('Ingrese el valor de la presion'))
+    if (presion>200) {
+        console.log('¡ALARMA LA PRESION ESTA DEMASIADA ALTA!');
+    }
+    else{
+        area=base*altura
+        console.log(`El area del rectangulo es ${area}`);
+    else if (temperatura>100){
+        console.log(`ALARMA LA TEMPERATURA ESTA DEMASIADO ALTA`);
+    }
+    else {
+        console.log('Los valores de temperatura y presion estan normales');
+    }
